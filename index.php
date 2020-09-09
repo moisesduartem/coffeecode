f84a47c0500bf2455bb0f12a1bbf64d1cf5d91b7
Moises Mariano <moisesduartem@gmail.com>
2020-09-09 11:08:56 -0300

    feat: exibe erros

--- index.php
+++ index.php
@@ -5,3 +5,30 @@ require_once __DIR__ . '/vendor/autoload.php';
 use CoffeeCode\Router\Router;
 
 $router = new Router(URL_BASE);
+
+/**
+ * Controllers
+ */
+$router->namespace('Source\App');
+
+/**
+ * Web @ home
+ */
+
+$router->group(null);
+$router->get('/', 'Web:home');
+
+/**
+ * Tratamento de erros no servidor
+ * (404, 401, 501, etc)
+ */
+
+$router->group('ooops');
+$router->get('/{errcode}', 'Web:error');
+
+$router->dispatch();
+
+if ($router->error()) {
+	$router->redirect("/ooops/{$router->error()}");
+}
+
